3fad2097a3deae4c8c306047fc643284d9be1f30
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2014-01-03 22:34:02 +0000

    Made pulling from wiki work\!\!\!

--- src/raptors.py
+++ src/raptors.py
@@ -11,26 +11,27 @@ print("""Welcome to the
 |  _ <  / ___ \|  __/ | || |_| |  _ <  | |_| |/ ___ \| |  | | |___ 
 |_| \_\/_/   \_\_|    |_| \___/|_| \_\  \____/_/   \_\_|  |_|_____|
                                                                     """)
+def inpt(prompt):
+    try: # Just another sign that python 3 sucks.  Deal.
+        c = raw_input(prompt)
+    except:
+        c = input(prompt)
+    return c
 
+# https://github.com/blog/699-making-github-more-open-git-backed-wikis
 if os.path.exists(os.path.expandvars("$RED_SPIDER_ROOT") + "/config/raptors"):
     print("You already have a copy of the raptor game data files.  Do you want to update it? (y/n)")
-    try: # Just another sign that python 3 sucks.  Deal.
-        c = raw_input("? ")
-    except:
-        c = input("? ")
+    c = inpt("? ")
     if c == "y":
         print("Downloading raptor game data, please wait...")
-        shutil.rmtree(os.path.expandvars("$RED_SPIDER_ROOT") + "/config/raptors")
+        shutil.rmtree(os.path.expandvars("$RED_SPIDER_ROOT") + "/config/raptors/")
         subprocess.call(["git", "clone", "https://github.com/WesleyAC/the-red-spider-project.wiki.git", os.path.expandvars("$RED_SPIDER_ROOT") + "/config/raptors"])
         print("Downloaded raptor game data!")
     else:
         print("Ok, keeping the game as is...")
 else:
     print("You do not have a copy of the raptor game data files.  Do you want to download them? (y/n)")
-    try: # Just another sign that python 3 sucks.  Deal.
-        c = raw_input("? ")
-    except:
-        c = input("? ")
+    c = inpt("? ")
     if c == "y":
         print("Downloading raptor game data, please wait...")
         subprocess.call(["git", "clone", "https://github.com/WesleyAC/the-red-spider-project.wiki.git", os.path.expandvars("$RED_SPIDER_ROOT") + "/config/raptors"])
@@ -38,3 +39,30 @@ else:
     else:
         print("You cannot play the raptor game without downloading the data.  Exiting...")
         exit()
+
+croom = "Raptor-Game"
+basedir = os.path.expandvars("$RED_SPIDER_ROOT") + "/config/raptors/"
+playing = True
+
+def printdata(room):
+    lines = room.split("\n")
+    chunks = room.split("~~")
+    print(lines[0])
+    print("")
+    print(chunks[1])
+
+def printops(room):
+    chunks = room.split("~~")
+    ops = chunks[2].split("\n")
+    print("Do you want to:")
+    i = 0
+    for op in ops[1:-1]:
+        print(chr(97+i) + ") " + op.split("::")[0])
+        i = i + 1
+
+while playing: # main loop
+    croomdata = open(basedir + croom + ".md")
+    croomtext = croomdata.read()
+    printdata(croomtext)
+    printops(croomtext)
+    c = inpt("")
